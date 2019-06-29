d52da71dd7e7fd30a5cc94b18e379f3f9e553ba0
nganhieu162005 <hieu.nguyen.489@my.csun.edu>
2019-06-28 17:14:22 -0700

    new change

--- Interview_Assignment/Controllers/HomeController.cs
+++ Interview_Assignment/Controllers/HomeController.cs
@@ -8,6 +8,7 @@
 
 	public class HomeController : Controller
 	{
+		
 		[HttpGet]
 		public ActionResult Index()
 		{
@@ -15,53 +16,52 @@
 		}
 
 		/// <summary>
-		/// return to View Success when File is converted to JSON or Error when Uploaded File is wrong 
+		/// Get file is converted to JSON or Error when Uploaded File is wrong 
 		/// </summary>
 		/// <param name="postedFileBase">uploaded Excel or CSV file</param>
-		/// <returns></returns>
+		/// <returns> return to View Success</returns>
 		[HttpPost]
 		public ActionResult Index(HttpPostedFileBase postedFileBase)
 		{
 			ViewBag.success = null;
 			try
 			{
-				// check file is existed or not
+				// Check file is existed or not
 				if (postedFileBase == null || postedFileBase.ContentLength <= 0)
 				{
-					ViewBag.error = "Please Select the Excel or CVS  file";
+					ViewBag.error = "The File could be not Excel or CSV file, or empty file. Please Select the correct Excel or CVS  file";
 				}
 				else
 				{
 					string[] validFileTypes = { ".xls", ".xlsx", ".csv" };
 					string extension = System.IO.Path.GetExtension(postedFileBase.FileName);
 
-					// check file extension is csv, xls, xlsx
+					// Check file extension is csv, xls, xlsx
 					if (validFileTypes.Contains(extension))
 					{
-						// store file in local system
-						// the path of converted folder
+						// Store file in local system
+						// The path of converted folder
 						string pathconvertedFolder = Server.MapPath("~/Content");
-						// the path of uploaded File
+						// The path of uploaded File
 						string pathUploadedFile = string.Format("{0}/{1}", pathconvertedFolder, postedFileBase.FileName);
 						bool result = false;
 
-						// remove existed file before create new one
+						// Remove existed file before create new one
 						if (System.IO.File.Exists(pathUploadedFile))
 						{
 							System.IO.File.Delete(pathUploadedFile);
 						}
-						postedFileBase.SaveAs(pathUploadedFile);
 
-						
+						postedFileBase.SaveAs(pathUploadedFile);
 						ConvertationFiles covertationFiles = new ConvertationFiles();
-						if (extension == ".csv")
+						if (extension.Equals(".csv"))
 						{
-							//convert CSV file to JSON and get result is True if  convert success
+							//Convert CSV file to JSON and get result is True if  convertion successes
 							result = covertationFiles.ConvertCSVToJSON(pathUploadedFile, pathconvertedFolder, postedFileBase.FileName);
 						}
 						else
 						{
-							//convert EXCEL file to JSON and get result is True if  convert success
+							//Convert EXCEL file to JSON and get result is True if  convertion successes
 							result = covertationFiles.ConvertXLSXToJSON(pathUploadedFile, pathconvertedFolder, postedFileBase.FileName);
 						}
 
@@ -72,19 +72,17 @@
 					}
 					else
 					{
-						ViewBag.error = "Please Select the Excel or CVS  file";
+						ViewBag.error = "The File could be not Excel or CSV file, or empty file. Please Select the correct Excel or CVS  file";
 					}
 				}
 			}
 			catch (Exception e)
 			{
 				Console.WriteLine(e);
-				ViewBag.error = "Please Select the Excel or CVS  file";
+				ViewBag.error = "The File could be not Excel or CSV file, or empty file. Please Select the correct Excel or CVS  file";
 			}
 			
 			return View();
 		}
-
-		
 	}
 }
